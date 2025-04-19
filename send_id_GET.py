b510abedfc1b541d3a18012980ed2c4fc7a0ec5e
hletzrin <90419129+hletzrin@users.noreply.github.com>
2025-04-19 19:06:47 -0400

    sending user IDs via post and get

---
+++ send_id_GET.py
@@ -0,0 +1,27 @@
+import network
+import urequests
+import time
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True) # activate the interface
+if not wlan.isconnected():
+    print("connecting to network...")
+    SSID = "Weida Apt 1-444 N Grant St"
+    Password = "NmvcSPgcwVm!"
+    wlan.connect(SSID,Password)
+    while not wlan.isconnected():
+        pass
+    print(f"Connected to {SSID}")
+    print(f"IP Address: {wlan.ifconfig()[0]}")
+
+user_id = "user1"
+
+server_ip = "192.168.5.47" # my compupter's id
+url = f"http://{server_ip}:8000/submit?key={user_id}"
+
+try:
+    response = urequests.get(url)
+    print("Response:", response.text)
+    response.close()
+except Exception as e:
+    print("Error sending request:", e)
