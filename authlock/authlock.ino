9191eea4e4211be4108e0a3ceddf7c35253ff64b
cmiotto20 <117414340+cmiotto20@users.noreply.github.com>
2025-05-04 14:24:31 -0400

    Update authlock.ino

--- authlock/authlock.ino
+++ authlock/authlock.ino
@@ -368,6 +368,23 @@ bool admin_pass() {
 
 
 /*-----------------------------FINGERPRINT SCANNER FUNCTIONS--------------------------------*/
+/***************************************************
+  This is an example sketch for our optical Fingerprint sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout
+  ----> http://www.adafruit.com/products/751
+
+  These displays use TTL Serial to communicate, 2 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Small bug-fix by Michael cochez
+
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
 uint8_t getFingerprintEnroll() 
 {
   int p = -1;
